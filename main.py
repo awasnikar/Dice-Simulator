7ac66e0bf5556b9f999941ea5a2660b4c0fb9990
awasnikar <66895450+awasnikar@users.noreply.github.com>
2020-11-27 20:33:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "Y" or roll_again == "y" or roll_again == "Yes" or roll_again == "YES" :
+    print("\nRolling the dice !")
+    time.sleep(2)
+
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+
+    print("The values are:")
+    print("Dice1=", dice1)
+    print("Dice2=", dice2)
+
+    if dice1 == dice2 :
+        print("You rolled a doubled !")
+    else:
+        print("Keep trying !")
+    roll_again = input("\nRoll the dice agaon? (Y/N)")
+
+
